f167c760d5b52582e27789face8c92f82a44e939
david_pb <david@rnet.mx>
2021-03-16 14:32:04 -0600

    Adds backend functionality

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
+import axios from 'axios';
 import Product from '../components/Product.js';
 
-import products from '../products.js';
-
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+
+    async function fetchProducts() {
+      const { data } = await axios.get('/api/products/');
+      setProducts(data);
+    }
+
+    fetchProducts();
+
+  }, [])
+
   return (
     <div>
       <h1>Latest Products</h1>
