7d74aa92bd87ada42272332e335abc546016c631
david_pb <david@rnet.mx>
2021-04-16 14:35:40 -0500

    Places order in front end

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
+from base.views import order_views
 
-urlpatterns = []
+urlpatterns = [
+    path('add/', view=order_views.addOrderItems, name='orders-add'),
+]
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -63,7 +63,9 @@ function CartScreen({ match, location, history }) {
                     <Form.Control
                       as="select"
                       value={item.qty}
-                      onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}
+                      onChange={(e) => dispatch(addToCart(
+                        item.product, Number(e.target.value)
+                      ))}
                     >
                       {
                         [...Array(item.countInStock).keys()].map((x) => (
